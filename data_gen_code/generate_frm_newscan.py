e0815065a49a0f54729883c1e873e6c5dc44398a
kushal <kushalv5678@gmail.com>
2018-04-23 08:29:53 -0500

    Modified the gaussian mean to accomodate separation for dwt

--- data_gen_code/generate_frm_newscan.py
+++ data_gen_code/generate_frm_newscan.py
@@ -34,30 +34,28 @@ for j in range(9):
 
 #Adding gaussian noise to simulate noise due to grain or due to system
 noise_added_signals=input_signal
-#div for different newscan 1-1,2-4,3-6,4-5,5-7,6-5,7-6,8-6,9-1,10-1,11-2,12-4,13-8,14-7
-div=7
+
+
 for signal in rotated_signals:
-    plt.figure()
-    plt.plot(sample_number,signal)
-    plt.xlabel('Sample Number---->')
-    plt.ylabel('Amplitude--------->')
-    plt.title('Rotated A-scan Plot')
-    plt.show()
-    std_dev=1
+    #plt.figure()
+    #plt.plot(sample_number,signal)
+    #plt.xlabel('Sample Number---->')
+    #plt.ylabel('Amplitude--------->')
+    #plt.title('Rotated A-scan Plot')
+    #plt.show()
+    std_dev=np.arange(1,5,0.4)
+    std_mean=np.arange(input_mean+50,input_mean+80,3)
     for k in range(1,11):
-        noise=[gauss(0,std_dev) for l in range(length_of_A_scan)]
+        noise=[gauss(std_mean[k-1],std_dev[k-1]) for l in range(length_of_A_scan)]
         temp_sig=signal+np.array(noise)
-        plt.figure()
-        plt.plot(sample_number,temp_sig)
-        plt.xlabel('Sample Number---->')
-        plt.ylabel('Amplitude--------->')
-        plt.title('Noise added A-scan Plot')
-        plt.show()
-        decision=input('Do you want to store the plot?')
-        if decision=='y':
-           noise_added_signals=np.vstack((noise_added_signals,temp_sig))
-        std_dev=float(input('Enter std deviation'))
-    std_dev=1 
+        #plt.figure()
+        #plt.plot(sample_number,temp_sig)
+        #plt.xlabel('Sample Number---->')
+        #plt.ylabel('Amplitude--------->')
+        #plt.title('Noise added A-scan Plot')
+        #plt.show()
+        noise_added_signals=np.vstack((noise_added_signals,temp_sig))
+    
 
 #disable comments upto plt.show() to display the synthetic data
 #for signal in noise_added_signals:   
